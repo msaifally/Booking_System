97835d80e4a728416a01948f7cd943080ffe3e67
msaifally <saifally@outlook.com>
2018-03-03 19:43:58 +0400

    main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'angular-calendar';
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
+import { LoginComponentComponent } from './Components/login-component/login-component.component';
+import { MainComponentComponent } from './Components/main-component/main-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
